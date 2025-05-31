1e48add6364fd951d7bce99d7582c5d1d401d0f1
agosarias <agosarias99@gmail.com>
2025-05-31 22:04:33 +0200

    add ft_printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,21 +6,41 @@
 /*   By: aarias-d <aarias-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/31 21:19:43 by aarias-d          #+#    #+#             */
-/*   Updated: 2025/05/31 21:43:12 by aarias-d         ###   ########.fr       */
+/*   Updated: 2025/05/31 22:03:42 by aarias-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-int	ft_printf(char const *, ...)
+int ft_validif(char const *str,va_list ap)
+{
+	if (!*str)
+		return (0);
+	if(*str == 'c')
+	{
+		ft_putchar_fd(str,1);
+		return(ft_strlen(ap));
+	}
+	return(1);
+}
+
+int	ft_printf(char const *str, ...)
 {
 	va_list ap;
 	int len;
+	
 	if(!ap)
 		return (0);
 	len = 0;
-	va_start(va_list ap,last);
-	va_arg(va_list ap, type);
-	write(1,"hola",4);
-	return(0);
+	va_start(ap,str);
+	while (*str)
+	{
+		if (*str++ == '%')
+		{
+			len += ft_validif(*str, ap);
+		}
+		len++;
+	}
+	va_end(ap);
+	return(len);
 }
